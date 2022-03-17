01b9a2e5ad4748e412f98c409ed8e7c3eaa14487
0sunset0 <chi6465618@naver.com>
2022-03-18 01:13:21 +0900

    commit

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -42,11 +44,21 @@ public class OrderService {
 
         //주문 저장
         orderRepository.save(order);
-
         return order.getId();
     }
 
+    /**
+     * 주문 취소
+     */
+    public void cancelOrder(Long orderId){
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
 
-
-
+//    //검색
+//    public List<Order> findOrders(OrderSearch orderSearch){
+//        return orderRepository.findAll(orderSearch)
+//    }
 }
